30f934886862f69ce68a1396051c0248ee15268c
Gowtham Dongari <gowthamdongari@gmail.com>
2023-09-06 08:11:11 -0500

    experience section update

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,17 +3,22 @@ import {motion, useScroll } from 'framer-motion'
 import LiIcon from './LiIcon'
 
 const Details = ({position, company, companyLink, time, address, work}) => {
+   const ref = useRef(null);
     return(
-     <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+     <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
         
-        <LiIcon/>
-        <div>
+        <LiIcon reference={ref}/>
+        <motion.div
+        initial={{y:50}}
+        whileInView={{y:0}}
+        transition={{duration:0.5, type:"spring"}}
+        >
             <h3 className='capitalize font-bold text-2xl' >
                 {position}&nbsp; 
                 <a 
                  href={companyLink}
                  target='_blank'
-                 className='text-dark capitalize'
+                 className='text-iris capitalize'
             >@{company}</a></h3>
             <span className='capitalize font-medium text-dark/75'>
                 {time} | {address}
@@ -21,7 +26,7 @@ const Details = ({position, company, companyLink, time, address, work}) => {
             <p className='font-medium w-full'>
                 {work}
             </p>
-        </div>
+        </motion.div>
     </li>
 )
 }
@@ -45,7 +50,7 @@ const Experience = () => {
 
 <motion.div 
 style={{scaleY: scrollYProgress}}
-className='absolute left-8 top-0 w-[4px] h-full bg-dark origin-top'/>
+className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top'/>
             <ul className='w-full flex flex-col items-start justify-between ml-4'>
                 
                 <Details
--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import {motion, useScroll} from "framer-motion"
 
-const LiIcon = () => {
+const LiIcon = ({reference}) => {
+  const {scrollYProgress} = useScroll(
+    {
+        target: reference,
+        offset: ["center end", "center center"]
+    }
+)
   return (
-    <figure>
-        <svg width='75' height='75' viewBox='0 0 100 100'>
-            
+    <figure className='absolute left-0 stroke-dark'>
+        <svg className='-rotate-90' width="75" height="75" viewBox='0 0 100 100'>
+            <circle cx="75" cy='50' r='20' className='stroke-iris stroke-1 fill-none'/>
+            <motion.circle cx="75" cy='50' r='20' className='stroke-[5px] fill-light'
+            style={{
+                pathLength :scrollYProgress
+            }}
+            />
+            <circle cx="75" cy='50' r='10' className='animate-pulse stroke-1 fill-iris'/>
         </svg>
+    </figure>
   )
-}
+}  
 
 export default LiIcon

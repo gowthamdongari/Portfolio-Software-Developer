337228802de332efe909dadbf99d67ab9b3a6812
Gowtham Dongari <gowthamdongari@gmail.com>
2023-09-11 10:28:29 -0500

    about & index update

--- src/pages/about.js
+++ src/pages/about.js
@@ -44,9 +44,10 @@ const about = () => {
     </Head>
     <main className='flex w-full flex-col items-center justify-center'>
         <Layout className='pt-16'>
-            <AnimatedText text="Learning Transforms into Real Work!" />
-            <div className='grid w-full grid-cols-8 gap-16 mt-16'>
-              <div className='col-span-3 flex flex-col items-start justify-start'>
+            <AnimatedText text="Learning Transforms into Real Work!" 
+            className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8' />
+            <div className='grid w-full grid-cols-8 mt-16 gap-16 sm:gap-8'>
+              <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'>
                 <h2 className='mb-4 text-lg font-bold text-dark/75'>
                   Unveiling My Unique Journey</h2>
                 <p className='font-medium'>
@@ -67,29 +68,41 @@ const about = () => {
                 </p>
               </div >
               <div className=' col-span-3 relative h-max rounded-2xl border-4 border-solid border-dark
-              bg-light p-8
+              bg-light p-8 xl:col-span-4 md:order-1 md:col-span-8
               '>
                 <div className='shadow-lg absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark'/>
-                  <Image src={profilePic} alt="Gowtham Dongari" className='w-full h-auto rounded-2xl'/>             
+                  <Image 
+                  src={profilePic} 
+                  alt="Gowtham Dongari" className='w-full h-auto rounded-2xl'
+                  priority
+                  sizes="(max-width: 768px) 100vw,
+                  (max-width: 1200px) 50vw,
+                  33vw"/>             
                 </div> 
-                <div className='col-span-2 flex flex-col items-end justify-between'>
-                  <div className='flex flex-col items-end justify-center'>
-                    <span className='inline-block text-7xl font-bold'>
+                <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3'>
+                  <div className='flex flex-col items-end justify-center xl:items-center'>
+                    <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                       <AnimatedNumbers value={10}/>+
                     </span>
-                    <h2 className='text-xl font-medium capitalize text-dark/75'>Projects Completed</h2>
+                    <h2 className='text-xl font-medium capitalize text-dark/75 xl:text-center
+                     md:text-lg sm:text-base xs:text-sm'>
+                      Projects Completed</h2>
                   </div>
-                  <div className='flex flex-col items-end justify-center'>
-                    <span className='inline-block text-7xl font-bold'>
+                  <div className='flex flex-col items-end justify-center xl:items-center'>
+                    <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                     <AnimatedNumbers value={2}/>
                     </span>
-                    <h2 className='text-xl font-medium capitalize text-dark/75'>Research Papers Published</h2>
+                    <h2 className='text-xl font-medium capitalize text-dark/75
+                    md:text-lg sm:text-base xs:text-sm'>
+                      Papers Published</h2>
                   </div>
-                  <div className='flex flex-col items-end justify-center'>
-                    <span className='inline-block text-7xl font-bold'>
+                  <div className='flex flex-col items-end justify-center xl:items-center'>
+                    <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                     <AnimatedNumbers value={2}/>+
                     </span>
-                    <h2 className='text-xl font-medium capitalize text-dark/75'>Years of Experience</h2>
+                    <h2 className='text-xl font-medium capitalize text-dark/75
+                    md:text-lg sm:text-base xs:text-sm'>
+                      Years of Experience</h2>
                   </div>
                 </div>
             </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,30 +16,39 @@ export default function Home() {
         
       </Head>
       <main className='flex items-center text-dark w-full min-h-screen'>
-        <Layout className='pt-0'>
-          <div className='flex items-center text-dark w-full min-h-screen'>
-            <div className='w-1/2 mr-6'>
-              <Image src={ProfilePic} alt="Gowtham Dongari" className='w-full h-auto'/>
+        <Layout className='pt-0 md:pt-16 sm:pt-8'>
+          <div className='flex items-center text-dark w-full min-h-screen lg:flex-col'>
+            <div className='w-1/2 mr-6 md:w-full'>
+              <Image src={ProfilePic} alt="Gowtham Dongari" className='w-full h-auto lg:hidden md:inline-block md:w-full'
+              priority
+              sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              50vw"
+              />
             </div>
-            <div className='w-1/2 flex flex-col items-center self-center'>
-              <AnimatedText text="Crafting code  from Syntax to Success." className='!text-3xl !text-left'/>
+            <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
+              <AnimatedText text="Crafting code  from Syntax to Success." className='!text-3xl !text-left 
+              xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl
+              '/>
               
               <p className='flex items-center mt-10 justify-center text-dark font-bold'>
                 Hi <span>&#128075;</span>, I am Gowtham Dongari</p>
-              <p className='my-4 text-base font-medium'>
+              <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>
                 I am a Master's student and budding Software Developer bringing ideas to life through imaginative web applications.
 Dive into my portfolio to discover my latest projects, all reflecting my evolving expertise in software development.
               </p>
-              <div className='flex items-center self-start mt-2'>
+              <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link href="/dummy.pdf" target={"_blank"}
                 className='flex items-center bg-black text-light p-2.5 px-6
                 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
-                border-2 border-solid border-transparent hover:border-dark'
+                border-2 border-solid border-transparent hover:border-dark 
+                md:p-2 md:px-4 md:text-base'
                 download={true}
                 >Resume <LinkArrow className={"w-6 ml-1"}/>
                 </Link>
                 <Link href="mailto:gowthamdongari@gmail.com" target={"_blank"}
-                className='ml-4 text-lg font-medium capitalize text-dark underline'
+                className='ml-4 text-lg font-medium capitalize text-dark underline 
+                md:text-base'
                 >Contact</Link>
               </div>
             </div>

692a161e83902212b21c373d38913761a3add594
Gowtham Dongari <gowthamdongari@gmail.com>
2024-07-20 14:05:53 -0500

    update meta tags for seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,9 +18,25 @@ export default function Home() {
       <Head>
         <title>Gowtham Dongari | Portfolio </title>
         <meta
-          name="Gowtham Dongari, Southern Methodist University, Dallas, Income Tax, Urban Rebox, Dongari Gowtham, software engineer, fullstack developer, open source"
-          content="Full Stack Developer"
+          name="description"
+          content="Gowtham Dongari, software developer, Southern Methodist University student masters, income tax, gowtham, dongari, full stack developer, open source, Dallas, Urban Rebox"
         />
+        <meta name="author" content="Gowtham Dongari" />
+        <meta
+          name="keywords"
+          content="Gowtham Dongari, software developer, Southern Methodist University, SMU, master's student, income tax, Urban Rebox, full stack developer, open source, Dallas"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="canonical" href="https://gowthamdongari.com" /> {/* Update with your actual URL */}
+        <meta property="og:title" content="Gowtham Dongari | Portfolio Page" />
+        <meta
+          property="og:description"
+          content="Gowtham Dongari, software developer, Southern Methodist University student masters, income tax, gowtham, dongari, full stack developer, open source, Dallas, Urban Rebox"
+        />
+        <meta property="og:url" content="https://gowthamdongari.com" /> {/* Update with your actual URL */}
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/public/images/profile/Professional_9.png" /> {/* Update with your actual image path */}
       </Head>
       <TransitionEffect />
       <main className="flex flex-col items-center text-dark w-full min-h-screen">
